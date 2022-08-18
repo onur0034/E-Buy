0b5e5f853d6cfb8b1d1a063cd9528631f92d6766
onur <onur.un0034@gmail.com>
2022-08-18 23:08:03 +0300

    first commit

---
+++ src/Category.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { ListGroup, ListGroupItem } from "reactstrap";
+
+export default class Category extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        { categoryId: 1, categoryName: "Alcohols" },
+        { categoryId: 2, categoryName: "foods" },
+      ],
+    };
+  }
+  render() {
+    return (
+      <div>
+        <h3>{this.props.info.title}</h3>
+
+        <ListGroup>
+          {this.state.categories.map((category) => (
+            <ListGroupItem key={category.categoryId}>
+              {category.categoryName}
+            </ListGroupItem>
+          ))}
+        </ListGroup>
+      </div>
+    );
+  }
+}
